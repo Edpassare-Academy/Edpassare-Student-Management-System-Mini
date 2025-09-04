290c8535e72a99842adb6e53520f0131fea26624
elofongo <elizabeth@edpassare.com>
2025-09-04 18:10:30 -0500

    I made the add_grade method. It adds a grade to the student's grade list.

--- student.py
+++ student.py
@@ -1,5 +1,14 @@
 class Student:
-    def __init__(self, name, age, student_id):
+    def __init__(self, name: str, age: int, student_id: str):
         self.name = name
         self.age = age
         self.student_id = student_id
+    
+    def add_grade(self, grade: float): 
+        self.grades = []
+        if grade >= 0 and grade <= 100:
+            self.grades.append(grade)
+        else:
+            print("Not a grade! Please put a number between 0 and 100!")
+
+    
