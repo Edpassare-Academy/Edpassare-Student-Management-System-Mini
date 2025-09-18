943aa4e9e00cf83fa30eef5b7c4aa627cdcd70ca
elofongo <elizabeth@edpassare.com>
2025-09-18 17:55:46 -0500

    I fixed the add_grade method, made the get_average method, the mark_attendance method, and the promote method!

--- student.py
+++ student.py
@@ -1,3 +1,13 @@
+from datetime import datetime
+
+#helper function
+def valid_date(idl: str):
+    try:
+        datetime.strptime(idl, "%Y-%m-%d")
+        return True
+    except ValueError:
+        return False
+
 class Student:
     def __init__(self, name: str, age: int, student_id: str):
         self.name = name
@@ -11,4 +21,30 @@ class Student:
         else:
             print("Not a grade! Please put a number between 0 and 100!")
 
-    
+    def get_average(self):   
+        total = 0
+        length = len(self.grades)
+        for x in self.grades: 
+            total += x
+        self.avg = total/length
+        return self.avg
+
+    def mark_attendance(self, dater: str):
+        self.attendance_record = []
+        result = valid_date(dater)
+        if result == True:
+            self.attendance_record.append(dater)
+        else:
+            return "Date format should be YYYY-MM-DD!"
+        
+
+    def promote(self):
+        if self.avg >= 60 and self.attendance_record >= 75:
+            self.promoted = True
+        else:
+            return "Not promoted."
+       
+        if self.promoted == True:
+            return f"{self.name}  is promoted!"
+        else:
+            return f"{self.name} isn't promoted."
